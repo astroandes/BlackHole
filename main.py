fd00aaba07d067dca049a40e7bd1ae376aba7e43
Christian Poveda <christianpoved4@gmail.com>
2014-10-20 22:02:51 -0500

    added arbitrary potential mode

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import numpy as np,sympy as sp, pylab
+from scipy.special import erf
 
-m = 1
-G = 1
+# Declares some global constants
 N = 3
+n_iter = 1000
+m = 1.0
+G = 1.0
+x = np.empty((n_iter,N))
+v = np.empty((n_iter,N))
+x[0] = np.array([0,1,0])
+v[0] = (2**(-0.5))*np.array([-1,0,0])
 
+
+# Defines a provisional potential
 def phi(x,y,z):
-    return G*m*m*(x*x+y*y+z*z)**(-0.5)
+    return -G*m*(x*x+y*y+z*z)**(-0.5)
 
-def integrate(D,x_0,v_0,dt,n_steps):
+# Euler integration method (provisional)
+def integrate(F,x_0,v_0,dt,n_steps):
     x = x_0
     v = v_0
     for i  in range(n_steps):
-        a = D(x,v)
+        a = F(x,v)/m
         x += v*dt
         v += a*dt
     return x,v
 
+# Calculates gradient of an arbitrary potential using sympy
 def getForce(Potential):
     x,y,z = sp.symbols('x y z')
+    print sp.diff(Potential(x,y,z),x)
     Fx = sp.lambdify((x,y,z),-sp.diff(Potential(x,y,z),x),'numpy')
     Fy = sp.lambdify((x,y,z),-sp.diff(Potential(x,y,z),y),'numpy')
     Fz = sp.lambdify((x,y,z),-sp.diff(Potential(x,y,z),z),'numpy')
@@ -25,6 +37,15 @@ def getForce(Potential):
 
 Fx,Fy,Fz = getForce(phi)
 
-def D(r,v):
+# Defines total forces: gravitational + dinamic friction
+def f(r,v):
     x,y,z = r
-    return np.array([Fx(x,y,z),Fy(x,y,z),Fz(x,y,z)])/m
+    b = np.sqrt(np.sum(v*v))
+    return np.array([Fx(x,y,z),Fy(x,y,z),Fz(x,y,z)])-0.0*(erf(b)-2.0*b*np.exp(-0.5*b*b))*v/b
+
+for i in range(n_iter-1):
+    x[i+1],v[i+1] = integrate(f,x[i],v[i],0.01,1)
+
+print x
+pylab.plot(x[:,0],x[:,1])
+pylab.show()
