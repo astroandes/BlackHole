4cb4fb1fcf5e86bf2b5a4d7bfa36f01fd37d30ac
Christian Poveda <christianpoved4@gmail.com>
2014-10-24 21:12:13 -0500

    wooooooorks :D

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
 import numpy as np,sympy as sp, pylab
 from scipy.special import erf
+from scipy.integrate import odeint as integrate
 
 # Declares some global constants
 N = 3
-n_iter = 7000
+n_iter = 5000
 m = 1.0
 rho_0 = 1.0
 G = 1.0
-x = np.empty((n_iter,N))
-v = np.empty((n_iter,N))
-x[0] = np.array([0,1,0])
-v[0] = (2**(0.5))*np.array([-1,0,0])
+s_0 = np.array([0,1,0,-2**(0.5),0,0]) #[x,y,z,v_x,v_y,v_z]
 
 # Defines a provisional potential
 def phi(x,y,z):
     return 2.0*np.pi*G*rho_0*sp.log(x*x+y*y+z*z)
-
-# Euler integration method (provisional)
-def integrate(F,x_0,v_0,dt,n_steps):
-    x = x_0
-    v = v_0
-    for i  in range(n_steps):
-        a = F(x,v)/m
-        x += v*dt
-        v += a*dt
-    return x,v
 
 # Calculates gradient of an arbitrary potential using sympy
 def getForce(Potential):
@@ -37,15 +25,31 @@ def getForce(Potential):
 
 Fx,Fy,Fz = getForce(phi)
 
-# Defines total forces: gravitational + dinamic friction
-def f(r,v):
-    x,y,z = r
+# Defines the time derivative of s: [v_x,v_y,v_z,a_x,a_y,a_z]
+def f(s,t):
+    x,y,z = s[:N]
+    v = s[N:]
     b = np.sqrt(np.sum(v*v))
-    return np.array([Fx(x,y,z),Fy(x,y,z),Fz(x,y,z)])-1.0*(erf(b)-2.0*b*np.exp(-0.5*b*b))*v/b
+    fx,fy,fz = -1.0*(erf(b)-2.0*b*np.exp(-0.5*b*b))*v/b
+    return np.array([v[0],v[1],v[2],(Fx(x,y,z)+fx)/m,(Fy(x,y,z)+fy)/m,(Fz(x,y,z)+fz)/m])
 
-for i in range(n_iter-1):
-    x[i+1],v[i+1] = integrate(f,x[i],v[i],0.001,1)
+t = np.linspace(0,20.0,n_iter)
+s = integrate(f,s_0,t)
 
-print x
+x,v = s[:,:N],s[:,N:]
+
+#E = 0.5*m*np.sum(v*v,axis=1)+2.0*np.pi*G*rho_0*np.log(x[:,0]*x[:,0]+x[:,1]*x[:,1]+x[:,2]*x[:,2])
+#print E
+
 pylab.plot(x[:,0],x[:,1])
-pylab.show()
+pylab.xlabel('$x$')
+pylab.ylabel('$y$')
+pylab.axes().set_aspect('equal','datalim')
+pylab.savefig('xy.png',dpi=200
+pylab.close()
+
+pylab.plot(t,np.sqrt(x[:,0]*x[:,0]+x[:,1]*x[:,1]))
+pylab.xlabel('$t$')
+pylab.ylabel('$r=\sqrt{x^{2}+y^{2}+z^{2}}$')
+pylab.savefig('r.png',dpi=200)
+pylab.close()
