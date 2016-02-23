404672d7cd749b420f9999a2587cdd319f4deff1
Christian Poveda <christianpoved4@gmail.com>
2016-02-23 17:13:52 -0500

    added new version

--- src/myModule/chandrasekhar.py
+++ src/myModule/chandrasekhar.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.special import erf
 
-def acceleration(r, v, m, G = 1, logLambda = 1, rho = lambda r_norm : 1, sigma = 1):
+def acceleration(r, v, m, G = 4490.0, logLambda = 1.0, rho = lambda r_norm : 1, sigma = 76.65):
     v_norm =  np.sqrt(np.sum(v*v))
     r_norm =  np.sqrt(np.sum(r*r))
     X =  v_norm/(np.sqrt(2)*sigma)
