cb48a007d895150274cc895abfb5588c8a4f282b
Christian Poveda <christianpoved4@gmail.com>
2016-02-24 11:30:46 -0500

    corrected velocity

--- src/myModule/halo.py
+++ src/myModule/halo.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def acceleration(r, v, m, v_h = 230160, q = 0.9, r_c = 0):
+def acceleration(r, v, m, v_h = 230.160, q = 0.9, r_c = 0):
     x, y, z = r
     accel = -v_h*v_h*r/(x*x+y*y+(z/q)*(z/q)+r_c*r_c)
     m_dot = np.zeros(1)
