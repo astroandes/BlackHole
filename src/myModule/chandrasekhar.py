12f036d9844654766515b21950188d0c1176d2ab
Christian Poveda <christianpoved4@gmail.com>
2016-02-24 11:30:12 -0500

    corrected normal term

--- src/myModule/chandrasekhar.py
+++ src/myModule/chandrasekhar.py
@@ -1,11 +1,11 @@
 import numpy as np
 from scipy.special import erf
 
-def acceleration(r, v, m, G = 4490.0, logLambda = 1.0, rho = lambda r_norm : 1, sigma = 76.65):
+def acceleration(r, v, m, G = 4490.0, logLambda = 1.0, rho = lambda r : 1, sigma = 76.65):
     v_norm =  np.sqrt(np.sum(v*v))
     r_norm =  np.sqrt(np.sum(r*r))
     X =  v_norm/(np.sqrt(2)*sigma)
-    accel = -4*np.pi*G*G*logLambda*m*v*rho(r_norm)*(erf(X)-2.*X*np.exp(-.5*X)/np.sqrt(np.pi))/v_norm**3
+    accel = -4*np.pi*G*G*logLambda*m*v*rho(r)*(erf(X)-2.*X*np.exp(-.5*X*X)/np.sqrt(np.pi))/v_norm**3
     m_dot = np.zeros(1)
     veloc = np.zeros(3)
     return np.concatenate((veloc,accel,m_dot))
