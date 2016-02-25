a0dcd591a370fc530e90280e5ee9737df08aa434
Christian Poveda <christianpoved4@gmail.com>
2016-02-25 10:36:20 -0500

    corrected constants

--- src/myModule/halo.py
+++ src/myModule/halo.py
@@ -8,8 +8,8 @@ def acceleration(r, v, m, v_h = 230.160, q = 0.9, r_c = 0):
     return np.concatenate((veloc,accel,m_dot))
 
 
-def density(r, v, m, v_h = 230.160, q = 0.9, r_c = 0):
+def density(r, G = 4490.0, v_h = 230.160, q = 0.9, r_c = 0):
     x, y, z = r
-    rho = 2*q*q*v_h*v_h*(a*a*q*q*(2*q*q+1)-2*q*q*q*q*(x*x+y*y)+q*q*(x*x+y*y+2*z*z)-3*z*z)
-    rho /= (a*a*q*q+q*q*(x*x+y*y)+z*z)**3
+    rho = 2*q*q*v_h*v_h*(r_c*r_c*q*q*(2*q*q+1)-2*q*q*q*q*(x*x+y*y)+q*q*(x*x+y*y+2*z*z)-3*z*z)
+    rho /= 4*np.pi*G*(r_c*r_c*q*q+q*q*(x*x+y*y)+z*z)**3
     return rho
